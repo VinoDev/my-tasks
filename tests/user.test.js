b36c65fff07cad2e395002008738a0449d1ed924
oleg <vinodevmail@gmail.com>
2020-09-28 17:53:57 +0300

    user signup and login tests, index.js restructure

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,36 @@
 const request = require('supertest');
+const app = require('../src/app.js');
+const User = require('../src/models/user.js');
 
-test('...', ()=>{
+const testUser = {
+    name: "Ron",
+    email: "rom@mail.com",
+    password: "rontest123"
+}
 
+beforeEach(async ()=>{
+    await User.deleteMany();
+    await new User(testUser).save();
+})
+
+test('Should signup a new user', async()=>{
+    await request(app).post('/users').send({
+        name: "Bob",
+        email: "bob@mail.com",
+        password: "test123"
+    }).expect(201)
+})
+
+test('Should login exsiting user', async ()=>{
+    await request(app).post('/users/login').send({
+        email: testUser.email,
+        password: testUser.password
+    }).expect(200)
+})
+
+test('Should NOT login if user dont exist', async ()=>{
+    await request(app).post('/users/login').send({
+        email: "idontexist@mail.com",
+        password: "idontexist123"
+    }).expect(400)
 })
